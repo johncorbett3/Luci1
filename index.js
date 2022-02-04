19c60d4574d2499f558a28c5f21c6fb4b6750e86
John Corbett <john.corbett@kellogg.northwestern.edu>
2022-02-04 14:12:07 -0600

    Attempting to print Sign Up inputs to console.log

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 
+// Attempting to print to console the user name (and eventually password) that gets submitted on Sign Up sheet
+console.log(document.getelementbyid('userName').value);
+
 function testFunction() {
 
 for (let i=0; i<10; i++) {
     console.log("x")
     }  
-}
+};
+
+
+// this is fron the "Sign up new users" code snippet here: https://firebase.google.com/docs/auth/web/start#web-version-9 
+    import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
