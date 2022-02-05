6d600f097d9384a97eb37bcc561c9a7971984e84
John Corbett <john.corbett@kellogg.northwestern.edu>
2022-02-05 14:12:24 -0600

    moved the print function outside HTML

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+function printConsole() {
 
-
+      var nameInput = document.getElementById('uname');
+      var passwordInput = document.getElementById('password');
+  
+      document.querySelector('form.pure-form').addEventListener('submit', function (e) {
+  
+      //prevent the normal submission of the form
+      e.preventDefault();
+  
+      console.log(nameInput.value);    
+      console.log(passwordInput.value);
+  });
+  }
 
 // this is fron the "Sign up new users" code snippet here: https://firebase.google.com/docs/auth/web/start#web-version-9 
     
